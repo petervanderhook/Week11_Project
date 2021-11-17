57f274118236b906b9c8a64b61a46c1e3d6092c1
petervanderhook <petervanderhook@gmail.com>
2021-11-17 13:07:36 -0800

    Added cleanUpIndex function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 /* This file is where all your work should go for Assignment 2.  Please remove this comment. */
+
+
+function cleanUpIndex() {
+    const clean = document.querySelectorAll()
+    clean.remove()
+}
