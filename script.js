f4fed1ecae5e264b6a53b7efc58aff648f793393
petervanderhook <petervanderhook@gmail.com>
2021-11-21 20:45:00 -0800

    Fixed cleanUpIndex function, it's working properly now

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 /* This file is where all your work should go for Assignment 2.  Please remove this comment. */
 
+const contactList = [  
+    { 
+        name: "Oliver Queen", 
+        phone: "778-555-1234", 
+        address: "101 Main St, Star City, USA",    
+        email: "greenarrow@watchtower.com",  
+    },   
+    {    
+        name: "Jessica Cruz",    
+        phone: "123-555-5555",    
+        address: "Portland Oregon",    
+        email: "greenlantern@watchtower.com",  
+    }
+]
+
 
 function cleanUpIndex() {
-    const clean = document.querySelectorAll()
-    clean.remove()
+    const main = document.querySelector('.main')
+    main.remove()
 }
 
 function createSingleIndex(contact) {
@@ -15,3 +30,30 @@ function createSingleIndex(contact) {
     } ]
     return contactList
 }
+
+
+function renderIndex(contact_array) {
+    let contact = document.querySelector('main')
+    for(let i = 0; i < contact_array.length; i++ ) {
+        console.log('testing ${contact_array[i]}')
+    }
+}
+
+/*function renderIndex(contact_array) {
+    for (i = 0; i++; i < length(contact_array) ) {
+        let contactcard = document.createElement("div")
+        contactcard.classList.add('contactinfo')
+        let contactname = contactcard.createElement("div")
+        let contactemail = contactcard.createElement("div")
+        let contactphone = contactcard.createElement("div")
+        let contactname = contactcard.createElement("div")
+        contactname.classList.add("contactname")
+        contactname.appendChild(contact_array[i].name)
+        contactemail.classList.add("contactemail")
+        contactname.appendChild(contact_array[i].email)
+        contactphone.classList.add("contactphone")
+        contactname.appendChild(contact_array[i].phone)
+        contactaddress.classList.add("contactaddress")
+        contactname.appendChild(contact_array[i].address)
+    }
+}*/
